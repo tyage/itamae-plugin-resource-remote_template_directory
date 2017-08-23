871e1fa05dbd2df5324aa23deff54c5caa879b40
tyage <namatyage@gmail.com>
2017-08-23 21:48:02 +0900

    init

---
+++ lib/itamae/plugin/resource/remote_template_directory.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/remote_template_directory/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module RemoteTemplateDirectory
+        # Your code goes here...
+      end
+    end
+  end
+end
